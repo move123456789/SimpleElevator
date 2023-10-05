cfc9efd42a6281005d171113f5fdd18720db85a7
SmokyAce <julian.hansen2002@gmail.com>
2023-10-06 01:40:51 +0200

    forgot to save

--- MonoNodePrefab.cs
+++ MonoNodePrefab.cs
@@ -57,43 +57,5 @@ namespace SimpleElevator
                 }
             }
         }
-
-        //internal class NodePrefabController : MonoBehaviour
-        //{
-        //    private Vector3 playerPos = LocalPlayer.Transform.position;
-        //    private bool isInside = false;
-        //    private float distance = 0f;
-        //    private float maxDistance = 2f;
-        //    internal StructureCraftingNode nodePrefabController;
-        //    internal GameObject nodePrefab;
-
-        //    void Update()
-        //    {
-        //        // Update the player's position
-        //        playerPos = LocalPlayer.Transform.position;
-
-        //        // Calculate distance between player and the nodePrefab
-        //        distance = Vector3.Distance(playerPos, nodePrefab.transform.position);
-
-        //        // Check if the distance is less than 2f and the player was previously outside
-        //        if (distance < maxDistance && !isInside)
-        //        {
-        //            nodePrefabController.GrabEnter();
-        //            isInside = true;
-        //        }
-        //        // Check if the player remains inside the boundary
-        //        else if (distance < maxDistance && isInside)
-        //        {
-        //            nodePrefabController.GrabStay();
-        //        }
-        //        // Check if the player moved out of the boundary
-        //        else if (distance >= maxDistance && isInside)
-        //        {
-        //            nodePrefabController.GrabExit();
-        //            isInside = false;
-        //        }
-        //    }
-
-        //}
     }
 }
